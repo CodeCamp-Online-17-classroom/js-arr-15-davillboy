adf13afcda16d75b8b3640832b0881aa41fd2ea3
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-23 19:09:54 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // write code here
+        function first(arr, n) {
+            if (n === undefined || n === 1) {
+              return arr[0];
+            } else if (n > 1) {
+              return arr.slice(0, n);
+            } else {
+              return [];
+            }
+          }
+          
+          console.log(first([7, 9, 0, -2])); // 7
+          console.log(first([], 3)); // []
+          console.log(first([7, 9, 0, -2], 3)); // [7, 9 ,0]
